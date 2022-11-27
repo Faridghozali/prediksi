914d6b279bfec620ef90e54d7e6161dc14ba88db
Datamining <111824123+Faridghozali@users.noreply.github.com>
2022-11-27 22:54:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,39 +3,24 @@ import pandas as pd
 import numpy as np
 import pickle
 
-st.write("""
-# Loan Prediction App
-This app predicts the **Palmer Penguin** species!
-Data obtained from the [loan prediction](https://github.com/Faridghozali/prediksi) in R by Allison Horst.
-""")
-
-st.sidebar.header('User Input Features')
-
-st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/Faridghozali/prediksi/main/data.csv)
-""")
-
-# Collects user input features into dataframe
-uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-if uploaded_file is not None:
-    input_df = pd.read_csv(uploaded_file)
-else:
-    def user_input_features():
-        sex = st.sidebar.selectbox('Sex',('male','female'))
-        applicants monthly income = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-        total loan amount = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-        credit_History = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-        data = {'sex': sex,
-                'Marital Status': Marital Status,
-                'Applicants monthly income': Applicants monthly income,
-                'Total loan amount': Total loan amount,
-                'Credit_History': Credit_History}
-        features = pd.DataFrame(data, index=[0])
-        return features
-    input_df = user_input_features()
-
-# Combines user input features with entire penguins dataset
-# This will be useful for the encoding phase
-penguins_raw = pd.read_csv('data.csv')
-penguins = penguins_raw.drop(columns=['Credit_History'])
-df = pd.concat([input_df,penguins],axis=0)
+def main():
+    st.title("kategori")
+    
+    menu=["Gender","Married","ApplicantIncome","LoanAmount","Credit_History"]
+    Choice= st.sidebar.selectbox("Menu",menu)
+     if Gender == "Male":
+        Gender = 0
+    else:
+        Gender = 1
+ 
+    if Married == "Unmarried":
+        Married = 0
+    else:
+        Married = 1
+ 
+    if Credit_History == "Unclear Debts":
+        Credit_History = 0
+    else:
+        Credit_History = 1  
+ 
+    LoanAmount = LoanAmount / 1000
