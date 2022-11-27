bc0223dc0258b6f2263a7a7dd0e82b2678570cd3
Datamining <111824123+Faridghozali@users.noreply.github.com>
2022-11-27 21:58:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pickle
 
 
 st.write("""
-# Penguin Prediction App
+# Loan Prediction App
 This app predicts the **Palmer Penguin** species!
 Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
 """)
@@ -23,18 +23,16 @@ if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
 else:
     def user_input_features():
-        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
         sex = st.sidebar.selectbox('Sex',('male','female'))
-        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-        data = {'island': island,
-                'bill_length_mm': bill_length_mm,
-                'bill_depth_mm': bill_depth_mm,
-                'flipper_length_mm': flipper_length_mm,
-                'body_mass_g': body_mass_g,
-                'sex': sex}
+        Marital Status= st.sidebar.selectbox('Marital Status',('Unmaried','maried'))
+        Applicants monthly income = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
+        Total loan amount = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
+        Credit_History = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
+        data = {'sex': sex,
+                'Marital Status': Marital Status,
+                'Applicants monthly income': Applicants monthly income,
+                'Total loan amount': Total loan amount,
+                'Credit_History': Credit_History}
         features = pd.DataFrame(data, index=[0])
         return features
     input_df = user_input_features()
